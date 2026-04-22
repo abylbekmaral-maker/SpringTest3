8f840cbab5ae5c67d475ef28b6a2584a492cca43
Maral <abylbekmaral@gmail.com>
2026-04-22 16:58:02 +0900

    first commit

---
+++ project_prerequisite-task_2_1_5/src/main/java/feast/models/Horse7.java
@@ -0,0 +1,16 @@
+package feast.models;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Horse7 {
+
+    @Autowired
+    private Camel8 camel8;
+
+    @Override
+    public String toString() {
+        return "which is stuffed inside a camel, "+camel8.toString();
+    }
+}
